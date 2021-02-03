eb588a38a72c0db3697df6210aeadccd4375a9b6
Adrian <ochala.adrian@gmail.com>
2021-02-03 21:38:20 +0100

    card testing

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,13 +1,28 @@
 <template>
-  <h1>card</h1>
+  <div class="card">
+    <img src="#" alt="Foto">
+    <div class="card__content">
+      <div class="card__content--title">
+        
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-name: "card"
-}
+  name: "card",
+};
 </script>
 
 <style lang="scss" scoped>
-
+@import "src/styles/abstract/variables";
+.card{
+  background-color: $color-grey-light;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 90%;
+}
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,38 +1,40 @@
 <template>
   <div class="home">
+    <h1>Headerek</h1>
+    <card></card>
+    <!--
     <div class="category" v-for="category in categories" v-bind:key="category.name">
       <p>{{ category.name }}</p>
     </div>
+    -->
   </div>
-
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
+import card from "../components/card";
 
 export default {
   name: "Home",
+  components: { card },
   computed: {
     ...mapGetters({
-      categories: 'categories/getCategories'
-    })
+      categories: "categories/getCategories",
+    }),
     // categories() {
     //   return this.$store.getters.categories();
     // }
-  }
-}
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  .home {
-    min-height: 100%;
-    background-color: orangered;
-
-  }
-  .category {
-    grid-row: start-content / end-content;
-
-    display: grid;
-
-  }
+.home {
+  min-height: 100%;
+  // background-color: orangered;
+}
+// .category {
+//   grid-row: start-content / end-content;
+//   display: grid;
+// }
 </style>
