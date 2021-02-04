063392f1f6e6ac28f4482da5cb5cf0e84c751497
Szymon Wójcik <wojcik.szymon1@gmail.com>
2021-02-04 14:46:18 +0100

    router prop test

--- src/router/index.js
+++ src/router/index.js
@@ -17,18 +17,18 @@ import addCategory from "@/views/admin/categories/addCategory";
 const routes = [
   { path: '/', component: home},
   { path: '/wycieczki', component: tripsCategories },
-  { path: '/wycieczki/:category', component: articleList },
-  { path: '/wycieczki/:category/:article', component: article },
+  { path: '/wycieczki/:category', component: articleList, props: true },
+  { path: '/wycieczki/:category/:article', component: article, props: true },
   { path: '/galeria', component: gallery },
   { path: '/o-nas', component: about },
   { path: '/admin', component: desktop },
   { path: '/admin/login', component: login },
   { path: '/admin/articles', component: list },
-  { path: '/admin/articles/:article/edit', component: editArticle},
-  { path: '/admin/articles/:article/add', component: addArticle},
+  { path: '/admin/articles/:article/edit', component: editArticle, props: true},
+  { path: '/admin/articles/:article/add', component: addArticle, props: true},
   { path: '/admin/category', component: listCategory },
-  { path: '/admin/category/:category/edit', component: editCategory},
-  { path: '/admin/category/:category/add', component: addCategory}
+  { path: '/admin/category/:category/edit', component: editCategory, props: true},
+  { path: '/admin/category/:category/add', component: addCategory, props: true}
 ]
 
 const router = createRouter({
--- src/views/trips/articleList.vue
+++ src/views/trips/articleList.vue
@@ -1,13 +1,20 @@
 <template>
-  <h1>Articles</h1>
+  <div>
+    <button @click="test">Test kategorii</button>
+  </div>
 </template>
 
 <script>
 export default {
-name: "ArticleList"
+  name: "ArticleList",
+  props: ['category'],
+  methods: {
+    test() {
+      console.log(this.category);
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
