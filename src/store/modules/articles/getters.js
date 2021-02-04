cfe04c8b9c3e942d6a0faf4e6ed6dc7ddbeaef4a
Szymon Wójcik <wojcik.szymon1@gmail.com>
2021-02-04 15:15:20 +0100

    uu

--- src/store/modules/articles/getters.js
+++ src/store/modules/articles/getters.js
@@ -1,5 +1,8 @@
 export default {
     getPosts(state) {
         return state.articles;
+    },
+    getCategoryPosts: (state) => (category) =>  {
+        return state.articles.filter((art) => art.category === category);
     }
 }
--- src/views/trips/articleList.vue
+++ src/views/trips/articleList.vue
@@ -1,20 +1,31 @@
 <template>
-  <div>
-    <button @click="test">Test kategorii</button>
+  <div class="article-list">
+    <div v-for="article in categoryArticles" :key="article.id">
+      {{ article.title }}
+    </div>
   </div>
 </template>
 
 <script>
+
+import {mapGetters} from "vuex";
+
 export default {
   name: "ArticleList",
   props: ['category'],
-  methods: {
-    test() {
-      console.log(this.category);
+  computed: {
+    ...mapGetters({
+      articles: 'articles/getCategoryPosts'
+    }),
+    categoryArticles() {
+      return this.articles(this.category);
     }
-  }
+}
 }
 </script>
 
 <style lang="scss" scoped>
+.article-list {
+
+}
 </style>
