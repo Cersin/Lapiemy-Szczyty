02bb939dab46c5aea0dc6773238c9a4c7a582fcf
Szymon Wójcik <wojcik.szymon1@gmail.com>
2021-03-24 13:45:01 +0100

    new

--- src/views/admin/articles/addArticle.vue
+++ src/views/admin/articles/addArticle.vue
@@ -1,8 +1,44 @@
 <template>
-  <div>
+  <form>
+    <div class="input">
+      <label for="title">Tytuł</label>
+      <input id="title" v-model="title" type="text">
+    </div>
+
+    <div class="input">
+      <label for="description">Opis</label>
+      <input id="description" v-model="description" type="text">
+    </div>
+
+    <div class="input">
+      <label for="category">Kategoria</label>
+      <input id="category" v-model="category" type="text">
+    </div>
+
+    <!--  TODO  -->
+    <div class="input">
+      <label for="mainPhoto">Główne zdjęcie</label>
+      <input id="mainPhoto" v-model="mainPhoto" type="text">
+    </div>
+
+    <div class="input">
+      <label for="tripDate">Data wycieczki</label>
+      <input id="tripDate" v-model="tripDate" type="date">
+    </div>
+
+    <div class="input">
+      <label for="duration">Czas</label>
+      <input id="duration" v-model="duration" type="time">
+    </div>
+
+    <div class="input">
+      <label for="distance">Dystans</label>
+      <input id="distance" v-model="distance" type="number">
+    </div>
+
     <ckeditor :editor="editor" v-model="editorData" :config="editorConfig"></ckeditor>
-    <h2>ELO</h2>
-  </div>
+    <button @click.prevent="addPost()">Add test</button>
+  </form>
 
 </template>
 
@@ -10,6 +46,7 @@
 import CKEditor from '@ckeditor/ckeditor5-vue';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import '@ckeditor/ckeditor5-build-classic/build/translations/pl';
+
 export default {
   name: "addArticle",
   components: {
@@ -17,12 +54,26 @@ export default {
   },
   data() {
     return {
+      title: null, // required
+      description: null, // required
+      category: null, // required
+      mainPhoto: null, // // required
+      images: null, // no required
+      tripDate: null, // required
+      duration: null, // no required
+      distance: null, // no required
+
       editor: ClassicEditor,
       editorData: '<p>Content of the editor.</p>',
       editorConfig: {
         // The configuration of the editor.
         language: 'pl'
       }
+    }
+  },
+  methods: {
+    addPost() {
+      console.log(this.tripDate);
     }
   }
 }
