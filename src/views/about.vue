b0e41acf421c2b56ae3be1a4d664667764765acb
Szymon Wójcik <wojcik.szymon1@gmail.com>
2021-04-06 15:54:21 +0200

    about

--- src/views/about.vue
+++ src/views/about.vue
@@ -2,11 +2,18 @@
   <div v-if="stats" class="about">
     <div class="czosnek">
       <img src="../assets/czosnek.png" alt="Zdjęcie Czonska">
-      <h1>Cześć! Nazywam się Czosnek!</h1>
-      <h2>I bardzo kocham góry!</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aperiam asperiores assumenda, atque deserunt
-        dicta eligendi esse illum molestias nobis obcaecati odit praesentium quas, repellendus repudiandae sequi
-        similique veniam vitae.</p>
+      <h1>Dzień dobry… Cześć… Ahoj!</h1>
+      <p class="text">Miałem iść czerwonym, nie jednak żółtym.. a może czarnym? Też tak czasem masz? 😊 Jednak jeżeli tutaj dotarłeś aż tutaj to: </p>
+      <ol type="a">
+        <li>Kochasz wędrówki górskie</li>
+        <li>Szukasz ciekawych wycieczek</li>
+        <li>Nie tak źle z czytaniem drogowskazów</li>
+      </ol>
+      <p class="text">
+        Będę tutaj właśnie dla Ciebie.  Ten blog powstał z myślą o amatorach pieszych wędrówek. Pomogę Ci odnaleźć ciekawe szlaki, gdzie nie tylko się napatrzysz, nachodzisz, ale także odpoczniesz na łonie natury. Będę umieszczał ładne fotki (nie tylko dlatego, że będę na nich ja), ale także żebyś złapał małą zajawkę i niczym się nie rozczarował. Dodatkowo umiem pisać, więc opiszę Ci moje doznania z wycieczki i umieszczę mapę z wędrówki żebyś mógł też zostawić swój ślad na tych ścieżkach. Może kiedyś się spotkamy na szlaku, więc komu w drogę temu łapy na szlak.
+
+      </p>
+      <h2>PS. ŁapieMY szczyty😉</h2>
     </div>
     <div class="stats">
       <h1>Liczba wycieczek</h1>
@@ -53,8 +60,23 @@ export default {
 }
 
 .czosnek {
-  text-align: center;
-  width: 60%;
+  width: 50%;
+  letter-spacing: .1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+
+  .text {
+    font-size: 1.4rem;
+  }
+
+  ol {
+    margin-top: 1rem;
+    margin-bottom: .5rem;
+    font-size: 1.3rem;
+    font-weight: 600;
+  }
 
   @media only screen and (max-width: 1000px) {
     width: 90%;
@@ -65,7 +87,6 @@ export default {
     border-radius: 30px;
     height: auto;
     width: 400px;
-
     @media only screen and (max-width: 700px) {
       width: 200px;
     }
@@ -77,7 +98,8 @@ export default {
   }
 
   h2 {
-    font-size: 1.8rem;
+    font-size: 2rem;
+    margin-top: 1rem;
     color: $color-primary-darker;
   }
 
@@ -87,7 +109,7 @@ export default {
 }
 
 .stats {
-  margin-top: 5rem;
+  margin-top: 3rem;
   text-align: center;
 
   h1 {
